e91906cc42c34f7f724028d715191bdf9c192402
gabriel-fox3 <gabriel@foxthree.com.br>
2020-04-09 17:11:37 -0300

    update

---
+++ header.php
@@ -0,0 +1,187 @@
+<header>
+  <div class="container-fluid header-top">
+    <div class="row justify-content-center row-top">
+      <div class="col-md-12 my-auto">
+        <div class="container">
+          <span class="float-left">Bem vindo a North Automotiva | <i class="mx-1 fab fa-whatsapp"></i> (51) 9 9999-9999</span>
+          <span class="float-right">
+            <i class="ml-3 mr-2 fas fa-map-marker-alt"></i> Onde estamos | 
+            <i class="ml-3 mr-2 fas fa-truck"></i> Acompanhe seu pedido | 
+            <i class="ml-3 mr-2 fas fa-shopping-bag"></i> Loja | 
+            <i class="ml-3 mr-2 fas fa-user"></i> Minha conta
+          </span>
+        </div>
+      </div>
+    </div>
+    <div class="container">
+      <div class="row row-header">
+        <div class="col-md-3 my-auto d-flex justify-content-center">
+          <a href="#" data-activates="slide-out" style="font-size: 22px" class="mr-4 transparent white-text p-3 my-auto button-collapse"><i class="fas fa-bars fa-1x"></i></a>
+          <div class="img-logo d-inline-block"></div>
+        </div>
+        <div class="col-md-6 my-auto">
+          <div class="input-group">
+            <input type="text" class="form-control input-search" placeholder="O que você procura?">
+            <div class="input-group-append">
+              <a href="" class="btn-lupa"><i class="fas fa-search"></i></a>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-3 my-auto text-right">
+          <a class="transparent dropdown-toggle mr-5" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="far fa-2x fa-user pr-2"></i></a>
+
+          <div class="dropdown-menu">
+            <a class="dropdown-item" href="#">Action</a>
+            <a class="dropdown-item" href="#">Another action</a>
+            <a class="dropdown-item" href="#">Something else here</a>
+            <div class="dropdown-divider"></div>
+            <a class="dropdown-item" href="#">Separated link</a>
+          </div>
+
+          <a class="transparent dropdown-toggle ml-2" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="fas fa-2x fa-shopping-bag"></i> <span class="ml-2 pr-2" style="font-size: 18px;">R$ 00,00</span></a>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="menu-fixed">
+    <div class="container h-100">
+      <div class="row justify-content-center h-100">
+        <div class="col-md-1 my-auto text-center">
+          <a href="#" data-activates="slide-out" style="font-size: 22px" class="transparent white-text p-3 my-auto button-collapse"><i class="fas fa-bars fa-1x"></i></a>
+        </div>
+        <div class="col-md-2 my-auto">
+          <div class="img-logo d-inline-block"></div>
+        </div>
+        <div class="col-md-6 my-auto">
+          <div class="input-group">
+            <input type="text" class="form-control input-search" placeholder="O que você procura?">
+            <div class="input-group-append">
+              <a href="" class="btn-lupa"><i class="fas fa-search"></i></a>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-3 my-auto text-right text-white">
+          <a class="transparent dropdown-toggle mr-5" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="far fa-2x fa-user pr-2"></i></a>
+
+          <div class="dropdown-menu">
+            <a class="dropdown-item" href="#">Action</a>
+            <a class="dropdown-item" href="#">Another action</a>
+            <a class="dropdown-item" href="#">Something else here</a>
+            <div class="dropdown-divider"></div>
+            <a class="dropdown-item" href="#">Separated link</a>
+          </div>
+
+          <a class="transparent dropdown-toggle ml-2" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="fas fa-2x fa-shopping-bag"></i> <span class="ml-2 pr-2" style="font-size: 18px;">R$ 00,00</span></a>
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+  <div id="slide-out" class="side-nav fixed elegant-color">
+    <ul class="custom-scrollbar">
+      <li>
+        <div class="logo-wrapper waves-light">
+          <a href="#"><img src="https://mdbootstrap.com/img/logo/mdb-transparent.png"
+              class="img-fluid flex-center"></a>
+        </div>
+      </li>
+      <li>
+        <ul class="social">
+          <li><a href="#" class="icons-sm fb-ic"><i class="fab fa-facebook-f"> </i></a></li>
+          <li><a href="#" class="icons-sm pin-ic"><i class="fab fa-pinterest"> </i></a></li>
+          <li><a href="#" class="icons-sm gplus-ic"><i class="fab fa-google-plus-g"> </i></a></li>
+          <li><a href="#" class="icons-sm tw-ic"><i class="fab fa-twitter"> </i></a></li>
+        </ul>
+      </li>
+      <li>
+        <form class="search-form" role="search">
+          <div class="form-group md-form mt-0 pt-1 waves-light">
+            <input type="text" class="form-control" placeholder="Search">
+          </div>
+        </form>
+      </li>
+      <li>
+        <ul class="collapsible collapsible-accordion">
+          <li><a class="collapsible-header waves-effect arrow-r"><i class="fas fa-chevron-right"></i> Submit
+              blog<i class="fas fa-angle-down rotate-icon"></i></a>
+            <div class="collapsible-body">
+              <ul>
+                <li><a href="#" class="waves-effect">Submit listing</a>
+                </li>
+                <li><a href="#" class="waves-effect">Registration form</a>
+                </li>
+              </ul>
+            </div>
+          </li>
+          <li><a class="collapsible-header waves-effect arrow-r"><i class="far fa-hand-pointer"></i>
+              Instruction<i class="fas fa-angle-down rotate-icon"></i></a>
+            <div class="collapsible-body">
+              <ul>
+                <li><a href="#" class="waves-effect">For bloggers</a>
+                </li>
+                <li><a href="#" class="waves-effect">For authors</a>
+                </li>
+              </ul>
+            </div>
+          </li>
+          <li><a class="collapsible-header waves-effect arrow-r"><i class="fas fa-eye"></i> About<i
+                class="fas fa-angle-down rotate-icon"></i></a>
+            <div class="collapsible-body">
+              <ul>
+                <li><a href="#" class="waves-effect">Introduction</a>
+                </li>
+                <li><a href="#" class="waves-effect">Monthly meetings</a>
+                </li>
+              </ul>
+            </div>
+          </li>
+          <li><a class="collapsible-header waves-effect arrow-r"><i class="far fa-envelope"></i> Contact me<i
+                class="fas fa-angle-down rotate-icon"></i></a>
+            <div class="collapsible-body">
+              <ul>
+                <li><a href="#" class="waves-effect">FAQ</a>
+                </li>
+                <li><a href="#" class="waves-effect">Write a message</a>
+                </li>
+                <li><a href="#" class="waves-effect">FAQ</a>
+                </li>
+                <li><a href="#" class="waves-effect">Write a message</a>
+                </li>
+                <li><a href="#" class="waves-effect">FAQ</a>
+                </li>
+                <li><a href="#" class="waves-effect">Write a message</a>
+                </li>
+                <li><a href="#" class="waves-effect">FAQ</a>
+                </li>
+                <li><a href="#" class="waves-effect">Write a message</a>
+                </li>
+              </ul>
+            </div>
+          </li>
+        </ul>
+      </li>
+    </ul>
+    <div class="sidenav-bg special-color"></div>
+  </div>
+</header>
+
+<script>
+  $(document).ready(() => {
+    // var header_h = $('.header-top').outerHeight();
+    var header_h = 210;
+    // console.log(header_h);
+    
+    $(window).scroll(() => {
+      if ($(window).scrollTop() >= header_h) {
+        // $('.header-top').slideUp();
+        $('.menu-fixed').slideDown();
+      } else {
+        // $('.header-top').slideDown();
+        $('.menu-fixed').slideUp();
+      }
+    });
+  });
+
+
+</script>
