7218a285f147eee8ddb870439c2688d44ac9ab47
gabriel-fox3 <gabriel@foxthree.com.br>
2020-04-09 16:56:09 -0300

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
-  <title>North Automotiva</title>
+  <title>Preview North Automotiva</title>
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css">
   <link href="css/bootstrap.min.css" rel="stylesheet">
   <link href="css/mdb.css" rel="stylesheet">
